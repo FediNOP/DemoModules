fea17beccbd1ced8a963203ea8c69cd61a198ede
Fedya Grigoryan <fedi7003@gmail.com>
2019-01-16 14:31:51 +0500

    Starting

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        try {
+
+            
+
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+
+
+
+
+    }
+
+
+
+}
