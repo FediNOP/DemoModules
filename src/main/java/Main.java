1714f7407f79ead2992c5db6f3d4724426d93573
Fedya Grigoryan <fedi7003@gmail.com>
2019-01-16 14:46:59 +0500

    TestOne

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
 
@@ -5,7 +8,14 @@ public class Main {
 
         try {
 
-            
+            Process proc = Runtime.getRuntime().exec("ls");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+            while(reader.readLine() != null){
+
+                System.out.println(reader.readLine());
+
+            }
 
 
         }catch (Exception e){
